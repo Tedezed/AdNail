4b4d9eccc3ab4adeae13dad94432101527619ca4
Juan Manuel Torres <zerrotchanel@gmail.com>
2014-03-16 20:06:37 +0100

    Update API eBay.py
    Primera salida en formato JSON

--- API eBay.py
+++ API eBay.py
@@ -1,5 +1,6 @@
 import webbrowser
 import requests
+import json
 from lxml import etree
 
 fil = open('Key.conf','r')
@@ -7,23 +8,37 @@ key = ''
 for lin in fil:
 	key = key + lin
 key = key.replace("\n","")
+fil.close()
 exec key
 
-url = 'http://open.api.ebay.com/shopping?'
-dicc = {'callname':'FindProducts','responseencoding':'XML',
-'appid':'','siteid':'0','version':'525','QueryKeywords':'',
-'AvailableItemsOnly':'true','MaxEntries':''}
+url = 'http://svcs.ebay.com/services/search/FindingService/v1?'
+dicc = {'OPERATION-NAME':'findItemsByKeywords',
+'SERVICE-VERSION':'1.0.0',
+'SECURITY-APPNAME':'',
+'RESPONSE-DATA-FORMAT':'JSON',
+'keywords':'',
+'paginationInput.entriesPerPage':''}
 
 #key = raw_input('Clave API: ')
-dicc['appid'] = appid
+dicc['SECURITY-APPNAME'] = appid
 
-
+#entrada = 'harry'
 entrada = raw_input('Palabras clave a buscar: ')
-dicc['QueryKeywords'] = entrada
+dicc['keywords'] = entrada
 
-numresp = raw_input('Numero de resultados: ')
-dicc['MaxEntries'] = numresp
+numresp = '3'
+#numresp = raw_input('Numero de resultados: ')
+dicc['paginationInput.entriesPerPage'] = numresp
 
 respuesta = requests.get(url,params = dicc)
-xml = etree.fromstring(respuesta.text.encode("utf-8"))
-print etree.tostring(xml)
+
+dicc = json.loads(respuesta.text.encode("utf-8"))
+print dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][0]['title'][0]
+print dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][0]['sellingStatus'][0]['currentPrice'][0]['@currencyId']
+print dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][0]['sellingStatus'][0]['currentPrice'][0]['__value__']
+print dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][1]['title'][0]
+print dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][1]['sellingStatus'][0]['currentPrice'][0]['@currencyId']
+print dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][1]['sellingStatus'][0]['currentPrice'][0]['__value__']
+print dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][2]['title'][0]
+print dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][2]['sellingStatus'][0]['currentPrice'][0]['@currencyId']
+print dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][2]['sellingStatus'][0]['currentPrice'][0]['__value__']
