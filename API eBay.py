5ca80deee4382c40d8c610bedfcbdc79390c9d3f
Juan Manuel Torres <zerrotchanel@gmail.com>
2014-03-16 20:49:34 +0100

    Update API eBay.py
    Añadido HTML

--- API eBay.py
+++ API eBay.py
@@ -1,7 +1,14 @@
 import webbrowser
 import requests
 import json
-from lxml import etree
+from jinja2 import Template
+
+fil = open('Plantilla.html','r')
+html = ''
+for linea in fil:
+	html = html + linea
+Plantilla = Template(html)
+fil.close()
 
 fil = open('Key.conf','r')
 key = ''
@@ -19,23 +26,29 @@ dicc = {'OPERATION-NAME':'findItemsByKeywords',
 'keywords':'',
 'paginationInput.entriesPerPage':''}
 
-#key = raw_input('Clave API: ')
 dicc['SECURITY-APPNAME'] = appid
-
-#entrada = 'harry'
 entrada = raw_input('Palabras clave a buscar: ')
 dicc['keywords'] = entrada
-
-#numresp = 3
 numresp = int(raw_input('Numero de resultados: '))
 dicc['paginationInput.entriesPerPage'] = numresp
 
 respuesta = requests.get(url,params = dicc)
 dicc = json.loads(respuesta.text.encode("utf-8"))
 
+imagurg = []
+title = []
+moneda = []
+precio = []
 contador = 0
 while numresp > contador:
-	print dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][contador]['title'][0]
-	print dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][contador]['sellingStatus'][0]['currentPrice'][0]['@currencyId']
-	print dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][contador]['sellingStatus'][0]['currentPrice'][0]['__value__']
+	imagurg.append(dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][contador]['galleryURL'][0])
+	title.append(dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][contador]['title'][0])
+	moneda.append(dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][contador]['sellingStatus'][0]['currentPrice'][0]['@currencyId'])
+	precio.append(dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][contador]['sellingStatus'][0]['currentPrice'][0]['__value__'])
 	contador = contador + 1
+
+Plantilla_sal = Plantilla.render(imagurgh=imagurg,titleh=title,monedah=moneda,precioh=precio)
+archi=open('Plantilla_sal.html','w')
+archi.write(Plantilla_sal)
+archi.close()
+webbrowser.open("Plantilla_sal.html")
