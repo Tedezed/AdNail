1528c752a3a63054b0b63de3a013995f2527a8aa
Xerrot <xerrot@XerrotDeb>
2014-04-07 23:15:50 +0200

    Servidor y estilo CSS

---
+++ ServerAPI.py
@@ -0,0 +1,75 @@
+from bottle import get, post, route, request, run, template, static_file
+import requests
+import requests
+import json
+
+iphost = raw_input('Introduce IP Server: ')
+
+fil = open('Key.conf','r')
+key = ''
+for lin in fil:
+    key = key + lin
+key = key.replace("\n","")
+fil.close()
+exec key
+
+#Archivos Estaticos
+@get('/css/<filename:re:.*\.css>')
+def sever_static(filename):
+    return static_file(filename, root='css')
+
+@get('/images/<filename:re:.*>')
+def sever_static(filename):
+    return static_file(filename, root='images')
+
+@route('/')
+def index():
+    return template('index.html')
+
+@get('/busqueda')
+def entrada():
+    return template('busqueda.html')
+
+@get('/contacto')
+def contacto():
+    return template('contacto.html')
+
+@post('/resultado')
+def salida():
+    try:
+        url = 'http://svcs.ebay.com/services/search/FindingService/v1?'
+        dicc = {'OPERATION-NAME':'findItemsByKeywords',
+        'SERVICE-VERSION':'1.0.0',
+        'SECURITY-APPNAME':'',
+        'RESPONSE-DATA-FORMAT':'JSON',
+        'keywords':'',
+        'paginationInput.entriesPerPage':''}
+
+        dicc['SECURITY-APPNAME'] = appid
+        entrada = request.forms.get('entrada')
+        dicc['keywords'] = entrada
+        numresp = 15
+        dicc['paginationInput.entriesPerPage'] = numresp
+
+        respuesta = requests.get(url,params = dicc)
+        dicc = json.loads(respuesta.text.encode("utf-8"))
+
+        imagurg = []
+        title = []
+        moneda = []
+        precio = []
+        contador = 0
+        while numresp > contador:
+            imagurg.append(dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][contador]['galleryURL'][0])
+            title.append(dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][contador]['title'][0])
+            monedac = dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][contador]['sellingStatus'][0]['currentPrice'][0]['@currencyId']
+            monedac = monedac.replace("USD", "$")
+            moneda.append(monedac)
+            precio.append(dicc['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][contador]['sellingStatus'][0]['currentPrice'][0]['__value__'])
+            contador = contador + 1
+
+        return template('resultado.html',imagurgh=imagurg,titleh=title,monedah=moneda,precioh=precio)
+    except KeyError:
+        return template('busqueda_error.html',entradah=entrada)
+
+run(host=iphost, port=8080, debug=True)
